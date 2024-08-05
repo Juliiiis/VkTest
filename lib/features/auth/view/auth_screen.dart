4822f0c83d4eb867a18eb64e891dc7181f19f4a3
kartapov_ba <kartapov_ba@magnit.ru>
2024-08-05 15:52:11 +0300

    stack and layout fix

--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vk/ui_kit/images/images.dart';
+import 'package:vk/ui_kit/tokens/icons/pet_icons.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -13,35 +14,53 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              SizedBox(
-                height: 39.h,
-                width: 39.w,
-                child: const Image(
-                  image: Images.pawLogo,
-                ),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+              height: 39.h,
+              width: 39.w,
+              child: const Image(
+                image: Images.pawLogo,
               ),
-              SizedBox(width: 8.w),
-              const Text(
-                'Happy Pet',
-              ),
-            ],
-          ),
+            ),
+            SizedBox(width: 8.w),
+            const Text(
+              'Happy Pet',
+            ),
+          ],
         ),
+      ),
       body: Stack(
         children: [
-          Positioned(
-            left: 187,
-            right: 0,
-            child: SizedBox(
-              width: 368.w,
-              height: 699.h,
-              child: const Image(image: Images.girlWithDog),
+          const Align(
+            alignment: Alignment.topRight,
+            child: Image(
+              image: Images.girlWithDog,
             ),
           ),
-          const Text('Login'),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 60.h,
+              top: 319.h,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Login'),
+                const Text('Please sign in to continue.'),
+                SizedBox(height: 61.h),
+                const Row(
+                  children: [
+                    Image(
+                      image: PetIcons.email,
+                    ),
+                    Text('EMAIL'),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
